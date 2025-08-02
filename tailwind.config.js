040073ef73cb807a5f1a1cc3233490b5c85143db
Neha <nehadas8762@gmail.com>
2025-08-02 09:44:55 +0000

    modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.html", "./src/**/*.{{html,js,jsx,ts,tsx,vue,astro}"],
+  content: [
+    "./public/**/*.html",
+    "./*/**/*.{html,js,jsx,ts,tsx,vue,astro}"
+  ],
   theme: {
-    extend: {
-      transitionProperty: {
-        "background-position": "background-position",
-      },
-      gridTemplateColumns: {
-        // 6 column grid
-        bento: "repeat(6, minmax(0, 1fr))",
-      },
-      gridRow: {
-        "span-2": "span 2 / span 2",
-        "span-3": "span 3 / span 3",
-      },
-      gridColumn: {
-        "span-2": "span 2 / span 2",
-        "span-3": "span 3 / span 3",
-      },
-    },
+    extend: {},
   },
   plugins: [
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/container-queries"),
-    require("prettier-plugin-tailwindcss"),
     require("@material-tailwind/html/utils/withMT"),
-    require("@tailwindcss/deprecation-warnings"),
   ],
 };
